14a49cfca2fc152e7f37d2b5f69a3fbb3b23c869
Austin Wonka <wonkaaustin@gmail.com>
2022-02-11 16:22:11 -0500

    hopefully finished commentjs and setup how html will look when viewing a post and wanting to leave a comment

---
+++ public/js/comment.js
@@ -0,0 +1,21 @@
+async function newFormHandler(event) {
+    event.preventDefault();
+
+    const commentContent = document.querySelector('#comment-content').value.trim();
+
+    if (commentContent) {
+        const response = await fetch('/api/comments', {
+            method: 'POST',
+            body: JSON.stringify({ commentContent }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+
+        if (response.ok) {
+            document.location.reload();
+        } else {
+            alert(response.statusText);
+        }
+    }
+};
+
+document.querySelector('#add-comment').addEventListener('submit', newFormHandler);
